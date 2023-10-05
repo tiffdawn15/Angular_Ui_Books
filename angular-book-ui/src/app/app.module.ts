ee513642d0dc243355d61ff44c5000929e1bce66
Tiffany Messer <tiffanymesser15@gmail.com>
2023-10-05 17:19:21 -0400

    The bootstrap table is now loading. Next step service class

--- angular-book-ui/src/app/app.module.ts
+++ angular-book-ui/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { BookTableComponent } from './book-table/book-table.component';
+
+// TODO: Alphabitize 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeComponent
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
