55f9ebbd34a563abccce36830303755f4963bcd0
Tiffany Messer <tiffanymesser15@gmail.com>
2023-10-13 13:47:51 -0400

    The table is populating with java api

--- angular-book-ui/src/app/app.module.ts
+++ angular-book-ui/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +18,9 @@ import { BookTableComponent } from './book-table/book-table.component';
     BookTableComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
     NgbModule,
     FormsModule
   ],
